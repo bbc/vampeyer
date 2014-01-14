083b3efd617e44c40dc5e4a6c26c29891382900c
Chris Baume <chris.baume@bbc.co.uk>
2014-01-14 16:53:14 +0000

    Changed colour to color so as not to confuse the yanks.

--- plugins/FreeSound.cpp
+++ plugins/FreeSound.cpp
@@ -16,34 +16,34 @@ private:
   // given a list of gradient values, return the value of a point
   // on that gradient
   //
-  // e.g. colour={1.0, 0.5, 0.0}
-  // colourmap(0.25, colour, 3) returns 0.75
-  double colourmap(double value, double colour[], int length)
+  // e.g. color={1.0, 0.5, 0.0}
+  // colormap(0.25, color, 3) returns 0.75
+  double colormap(double value, double color[], int length)
   {
     int iter=0;
     double step = 1.0/(double)length;
     if (value>1) return 0;
     while (value > step*(iter+1)) iter++;
     double weight = (value-step*(double)iter)/step;
-    return (1.0-weight)*colour[iter] + weight*colour[iter+1];
+    return (1.0-weight)*color[iter] + weight*color[iter+1];
   }
 
   double red(double value)
   {
-    double colours[] = PALETTE_RED;
-    return colourmap(value, colours, PALETTE_LENGTH);
+    double colors[] = PALETTE_RED;
+    return colormap(value, colors, PALETTE_LENGTH);
   }
 
   double green(double value)
   {
-    double colours[] = PALETTE_GREEN;
-    return colourmap(value, colours, PALETTE_LENGTH);
+    double colors[] = PALETTE_GREEN;
+    return colormap(value, colors, PALETTE_LENGTH);
   }
 
   double blue(double value)
   {
-    double colours[] = PALETTE_BLUE;
-    return colourmap(value, colours, PALETTE_LENGTH);
+    double colors[] = PALETTE_BLUE;
+    return colormap(value, colors, PALETTE_LENGTH);
   }
 
 public:
@@ -79,7 +79,7 @@ public:
       unsigned int scFrames = features[1].size();
       double scScale = (double)scFrames/(double)peakFrames;
 
-      // for each peak frame, draw a coloured line
+      // for each peak frame, draw a colored line
       for (unsigned int peakFrame=0; peakFrame<peakFrames; peakFrame++)
       {
         // find spectral contrast, clip and scale
