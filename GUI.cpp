ae455f8d86fc34786f578236aab79e4df1e7e319
Chris Baume <chris.baume@bbc.co.uk>
2014-05-02 12:16:40 +0100

    Fixed problem where FLTK was rendering colours incorrectly.

--- GUI.cpp
+++ GUI.cpp
@@ -5,6 +5,17 @@ GUI::GUI(int width, int height, unsigned char *buffer)
   winWidth=width;
   winHeight=height;
 
+  // convert ARGB to ABGR
+  // TODO Work out why this is necessary
+  unsigned int* buf = (unsigned int*)&buffer[0];
+  unsigned int* end = buf + width*height;
+  while(buf < end) {
+    unsigned int pixel = *buf;
+    *buf = ((pixel&0x00ff0000)>>16) + ((pixel&0x000000ff)<<16) +
+      ((pixel&0xff00ff00));
+    buf++;
+  }
+
   Fl::visual(FL_RGB);
   win = new Fl_Window(winWidth, winHeight, "AudioVis");
   win->position((Fl::w() - win->w())/2, (Fl::h() - win->h())/2);
